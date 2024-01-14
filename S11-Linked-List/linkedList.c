2183a3c2553101d554d24119ef17d90ef554e644
Matheus Muniz <matheusmuniz215@gmail.com>
2024-01-14 18:58:54 -0300

    creating and displaying linked list

---
+++ S11-Linked-List/linkedList.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// What do you want to do in this program?
+
+// Create a linked list and display it
+
+// Create linked list from array
+
+struct Node
+{
+    int data;
+    struct Node *nextNode;
+};
+
+void *createLinkedList(int *array, int length)
+{
+    // Allocating memory for the first node
+    struct Node *first = (struct Node *)malloc(sizeof(struct Node));
+
+    struct Node *currentNode;
+
+    first->data = array[0];
+    first->nextNode = (struct Node *)malloc(sizeof(struct Node));
+
+    currentNode = first->nextNode;
+
+    for (int i = 1; i < length - 1; i++)
+    {
+        currentNode->data = array[i];
+        currentNode->nextNode = (struct Node *)malloc(sizeof(struct Node));
+        currentNode = currentNode->nextNode;
+    };
+
+    currentNode->data = array[length - 1];
+    currentNode->nextNode = NULL;
+
+    return first;
+};
+
+void displayLinkedList(struct Node *first)
+{
+    struct Node *currentNode = first;
+
+    while (currentNode->nextNode != NULL)
+    {
+        printf("%d\n", currentNode->data);
+        currentNode = currentNode->nextNode;
+    }
+};
+
+int main(void)
+{
+    int numbers[] = {2, 4, 6, 8, 10};
+
+    struct Node *first = createLinkedList(numbers, 5);
+    displayLinkedList(first);
+
+    return 0;
+}
